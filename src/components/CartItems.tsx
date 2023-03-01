6d0169d5743ab7b2afa72c1e3d22cb524b8e024b
arundevu <arunitech09@gmail.com>
2023-03-01 14:57:05 +0530

    some-changes

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -2,20 +2,30 @@ import axios from "axios";
 import { log } from "console";
 import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import { Link } from "react-router-dom";
 import { productApi } from "../Utility/Environment";
 
 export const CartItems=()=>{
     const[Itemsdata,setItemData]=useState<any>([]);
     const[priceDetails,setPriceDetails]=useState<any>();
     const[totalPrice,setTotalPrice]=useState<any>();
+    const[totalprices , setTotalPrices1] = useState(0);
+    let netamt: any;
 const [id,setId]=useState(null);
 const [qty,setQty]=useState(0);
-console.log(Itemsdata,"cartdala")
+console.log(Itemsdata,"cartdala");
+var data :any;
+
+const allProAmt = Itemsdata.reduce((preVal: any,currentVal: { price: any; })=>{
+    return preVal + currentVal.price;
+},0)
 const RemoveCart = (i:any)=>{
-    console.log(i,"lllll")
+    // console.log(i,"lllll")
 Itemsdata.splice(i,1)
- localStorage.setItem("data",JSON.stringify(Itemsdata)) 
+ localStorage.setItem("data",JSON.stringify(Itemsdata));
 setId(i)
+
+
 
 }
 useEffect(()=>{
@@ -23,18 +33,19 @@ useEffect(()=>{
     var cartData =  localStorage.getItem("data");
     if(cartData){
     const data =  JSON.parse(cartData);
+    // console.log(data, "data initial");
     setItemData(data)
 
     }
+
     
 },[id]);
-
+var pr =0
 const qtyDecInc = (e:any , val:any)=>{
-    const myData:any[] = []
+    const myData:any[] = [];
+
     Itemsdata.forEach((res:any , i:any)=>{
-        console.log(res,"resss")
         if(res.id === e.id){
-            console.log(val,"valll")
             if(val ==='INC'){
               res.qty++
              
@@ -44,17 +55,18 @@ const qtyDecInc = (e:any , val:any)=>{
             } 
             const data = JSON.parse(localStorage.getItem("data")||"")
             data.forEach((items:any)=>{
-               console.log(items , "itemss")
+            //    console.log(items , "itemss")
                if(items.id === res.id){
-                res.price =items.price*res.qty
-                setTotalPrice(res.price)
+                res.price =items.price*res.qty;
 
-
+                console.log(res.price , "pricesssssssss");
+               pr+= items.price;
+            //    console.log(pr,"prrrr")
+                setTotalPrice(pr);
                }
                return items;
             })
             setQty(res.qty)
-            
         }
         console.log(priceDetails,"priceDetails")
         myData.push(res)
@@ -62,6 +74,8 @@ const qtyDecInc = (e:any , val:any)=>{
         setItemData(myData)
     })
 }
+var tttl =pr;
+console.log(tttl,"ttttlll")
 const QtyIncreament =(e:any)=>{
     qtyDecInc(e ,'INC')
     console.log(e,"eee")
@@ -71,20 +85,20 @@ const QtyDecreament =(e:any)=>{
     }
 
 
-
     return<>
     <div className="container-fluid" style={{marginTop:"154px"}}>
             
              <div  className="row ">
                 {Itemsdata.map((res:any,i:any)=>{
+                    console.log(res.price,"price")
                     return<>
-                     <aside className="col-lg-9 " key={i}>
+                     <aside className="col-lg-12" key={i}>
                 <div className="card">
                     <div className="table-responsive">
                         <table className="table table-borderless table-shopping-cart">
                             <thead className="text-muted">
                                 <tr className="small text-uppercase">
-                                    <th scope="col">Product</th>
+                                    <th scope="col-lg-2">Product</th>
                                     <th scope="col" style={{width:120}} >Quantity</th>
                                     <th scope="col"style={{width:120}}>Price</th>
                                     <th scope="col" className="text-right d-none d-md-block" style={{width:200}}></th>
@@ -96,7 +110,8 @@ const QtyDecreament =(e:any)=>{
                                         <figure className="itemside align-items-center">
                                             <div className="aside">
                                                 <img src={res.image} className="img-sm" width={70} height={70} /></div>
-                                            <div className="info"> <a href="#" className="title text-dark" data-abc="true">{res.title}</a>
+                                            <div className="info">
+                                                {/* <a href="#" className="title text-dark" data-abc="true">{res.title}</a> */}
                                         
                                                
                                             </div>
@@ -125,13 +140,17 @@ const QtyDecreament =(e:any)=>{
                     </>
                 })}
            
-            <aside className="col-lg-3">
+           
+        </div>
+           
+        <aside className="col-lg-12 mt-5">
                
+                
                <div className="card">
                    <div className="card-body">
                        <dl className="dlist-align">
                            <dt>Total price:</dt>
-                           <dd className="text-right ml-3">$69.97</dd>
+                           <dd className="text-right ml-3">${allProAmt}</dd>
                        </dl>
                        <dl className="dlist-align">
                            <dt>Discount:</dt>
@@ -139,15 +158,13 @@ const QtyDecreament =(e:any)=>{
                        </dl>
                        <dl className="dlist-align">
                            <dt>Total:</dt>
-                           <dd className="text-right text-dark b ml-3"><strong>$59.97</strong></dd>
+                           <dd className="text-right text-dark b ml-3"><strong>${allProAmt-10 ==-10 ? 0: allProAmt-10 }</strong></dd>
                        </dl>
-                       <hr/> <a href="#" className="btn btn-out btn-primary btn-square btn-main" data-abc="true"> Make Purchase </a> <a href="#" className="btn btn-out btn-success btn-square btn-main mt-2" data-abc="true">Continue Shopping</a>
+                       <hr/>  <Link to="/Product" className="btn btn-out btn-success btn-square btn-main mt-2" data-abc="true">Make Purchase & Continue Shopping</Link>
                    </div>
                </div>
+               
            </aside>
-        </div>
-           
-        
        
     </div>
     </>
---
+++ src/components/Loder.tsx
@@ -0,0 +1,13 @@
+import { Spin } from "antd";
+import React from "react";
+
+
+export const Loader =()=>{
+ 
+    
+    return<>
+      <div className="loader">
+    <Spin />
+  </div>
+    </>
+}
